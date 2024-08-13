af4db01ccd49f35cc4b087b00a4c5181135a4bd3
MEBARKI Mehdi <mehdi-mebarki@live.fr>
2024-08-13 04:28:12 +0200

    create migration for invoices table

---
+++ database/migrations/2024_08_13_042655_create_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')->constrained()->onDelete('cascade'); // Clé étrangère vers la table reservations
+            $table->string('subject'); // Objet de la facture
+            $table->text('description'); // Désignation ou détails supplémentaires
+            $table->timestamp('issued_at')->nullable(); // Date d'émission de la facture
+            $table->timestamp('paid_at')->nullable(); // Date de paiement de la facture
+            $table->enum('status', ['pending', 'issued', 'paid'])->default('pending'); // Statut de la facture
+            $table->timestamps(); // created_at et updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
